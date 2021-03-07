2e6c73c432a3d9905f1e9d99aea91bef79903b31
HideOnDani <daniel352001@gmail.com>
2021-03-07 16:53:31 +0100

    Cambio Dani

--- Actividad GIT IDE/src/main/Main.java
+++ Actividad GIT IDE/src/main/Main.java
@@ -4,5 +4,6 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Esto es un cambio en el proyecto");
 		System.out.println("Cambio Alejandro");
+		System.out.println("Cambio Dani");
 	}
 }
