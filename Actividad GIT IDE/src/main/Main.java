d060b70383dc382d879661ce877721ae983294cf
Diego <djmdi5c@gmail.com>
2021-03-07 17:05:04 +0100

    Cambio Diego Terán.

--- Actividad GIT IDE/src/main/Main.java
+++ Actividad GIT IDE/src/main/Main.java
@@ -5,6 +5,6 @@ public class Main {
 		System.out.println("Esto es un cambio en el proyecto");
 		System.out.println("Cambio Alejandro");
 		System.out.println("Cambio Dani");
-		System.out.println("Cambio Diego Terán");
+		System.out.println("Cambio Diego T");
 	}
 }
